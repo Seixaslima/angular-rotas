ef5c227997b6e3b227d38ceeae3b5c5180b45e02
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-01-11 20:50:00 -0300

    rotas configuradas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { PortifolioComponent } from './pages/portifolio/portifolio.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: IndexComponent, pathMatch:"full"},
+  {path:'portifolio', component: PortifolioComponent },
+  {path:'portfolio', redirectTo: 'portifolio' },
+  {path:'**', component: NotFoundComponent}
+];
